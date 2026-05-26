a573ec87235ad5ae13749acd6cdda43693334a36
mondalaniket16 <mondalaniket16@gmail.com>
2026-05-27 01:12:53 +0530

    doubly linkedlist implementation

---
+++ Doubly LinkedList/Implementation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Main
+{
+    public:
+    void function()
+    {
+        
+    }
+};
+int main()
+{
+    Main ob;
+    int n;
+    cin>>n;
+    ob.function();
+    return 0;
+}
